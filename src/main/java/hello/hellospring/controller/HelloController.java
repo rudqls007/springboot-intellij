295d630ee22a70add45a840fd12d9024dbda1043
rudqls007 <rudqls007@gmail.com>
2023-02-13 14:07:19 +0900

    정적 컨텐츠 - 정적인 상태 그대로 출력
    MVC 템플릿 엔진 컨텐츠 - Model, View, Controller 방식으로 쪼개서 출력
    
    api 컨텐츠 - 객체를 반환함 ( JSON 타입으로 ) view 없이 @ResponesBody 어노테이션을 사용함.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,4 +24,34 @@ public class HelloController {
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name; // hello spring html 태그 없이 데이터 값만 그대로 출력된다.
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody   // json으로 반환하는 게 기본임.
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello{
+        private String name;
+        // alt + insert = getter / setter 단축기
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+
+    }
 }
